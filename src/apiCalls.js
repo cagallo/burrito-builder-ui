87594a0255255fc07984ce228012bf0d4f621caa
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-08 09:38:04 -0700

    Add post to api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,6 +3,18 @@ export const apiCalls = {
   async getOrders() {
   let response = await fetch('http://localhost:3001/api/v1/orders')
     return await response.json();
-  }
-  
+  },
+
+  async postOrder(order) {
+    let response = await fetch('http://localhost:3001/api/v1/orders', {
+        method: 'POST',
+        body: JSON.stringify(order),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      return await response.json();
+    }
 }
+
+export default apiCalls;
