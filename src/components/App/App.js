134f53f269429ba2c52f2367203969a36b42ba12
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-08 11:51:39 -0700

    Add delete function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,15 +23,34 @@ class App extends Component {
     }
   }
 
+  addOrder = userOrder => this.setState({orders: [...this.state.orders, userOrder]})
+
+  deleteUserOrder = async(id) =>  {
+    // console.log(deleteIndex)
+    const currentOrders = [...this.state.orders]
+    console.log(currentOrders)
+    const deleteIndex = currentOrders.findIndex(order => order.id === id)
+    currentOrders.splice(deleteIndex, 1)
+    console.log(currentOrders.splice(deleteIndex, 1))
+    try {
+      let data = await apiCalls.deleteOrder(id)
+      this.setState({ orders: data.orders })
+    }
+    catch(error) {
+      this.setState({ error: error.message })
+    }
+  }
+
   render() {
+    const orderInfo = this.state.orders.length > 0 && <Orders orders={this.state.orders} deleteUserOrder={this.deleteUserOrder} />
     console.log(this.state.orders)
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm addOrder={this.addOrder} />
         </header>
-        <Orders orders={this.state.orders}/>
+        {orderInfo}
       </main>
     );
   }
