366fbcf02b2b976eb9944f3edc5b687c06627b42
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-08 10:49:13 -0700

    Get map functional

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+const Orders = ( props ) => {
+  if(!props.orders) {
+    return ''
+  }
+  console.log(typeof props.orders.orders)
+  let orderEls = props.orders.map(order => {
     return (
-      <div className="order">
+      <div className="order" id={order.id} key={order.id}>
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}``
+            return <li key={`${ingredient}_${Date.now()}`}>{ingredient}</li>
+          })}
         </ul>
+        {/* <button id={order.id} onClick ={(e) => props.deleteOrder(e.target.id)}>DELETE</button> */}
       </div>
     )
   });
