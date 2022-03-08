5a59771511d8b56904560b1f87cdd6c1ae200f82
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-08 09:33:37 -0700

    Add get request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,8 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+export const apiCalls = {
+
+  async getOrders() {
+  let response = await fetch('http://localhost:3001/api/v1/orders')
+    return await response.json();
+  }
+  
 }
