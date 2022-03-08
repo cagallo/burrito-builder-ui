f30cc4e5e65e3e87d3aacaf25b76cc5fd46b734e
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-08 09:33:56 -0700

    Refactor component did mount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {apiCalls} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: [],
+      error: ''
+    }
   }
 
-  componentDidMount() {
-    getOrders()
-      .catch(err => console.error('Error fetching:', err));
+  componentDidMount = async() => {
+    try {
+      let orderInfo = await apiCalls.getorders();
+      this.setState({orders: orderInfo})
+    }
+    catch(error) {
+      this.setState({error: error.message})
+    }
   }
 
   render() {
@@ -21,7 +30,6 @@ class App extends Component {
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
-
         <Orders orders={this.state.orders}/>
       </main>
     );
