79ff6ef272548c9be75d51613006f1dc5cb797bd
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-08 13:06:48 -0700

    Add tests for burrito builder spec

---
+++ cypress/integration/burrito_builder_spec.js
@@ -0,0 +1,59 @@
+describe('Burrito Builder user flows', () => {
+  beforeEach(() => {
+    cy.intercept('http://localhost:3001/api/v1/orders', { fixture: 'singleOrder.json' });
+    cy.visit('http://localhost:3000');
+  })
+
+  it('should allow users to see submitted orders on page load', () => {
+    cy.get('.header').contains('Burrito Builder')
+    cy.get('.order-container')
+    cy.get('.order').should('have.length', 1)
+      .get('.order').first().contains('h3', 'Pat')
+    cy.get('.ingredient-list').contains('li', "beans")
+      .get('.ingredient-list').contains('li', "lettuce")
+      .get('.ingredient-list').contains('li', "carnitas")
+      .get('.ingredient-list').contains('li', "queso fresco")
+      .get('.ingredient-list').contains('li', "jalapeno")
+    cy.get('.delete-button').contains('DELETE')
+  })
+  
+  it('should display a form where users can create their own burrito', () => {
+    cy.get('form')
+    cy.get('input[name="name"]')
+      .type('Chez')
+      .should('have.value', 'Chez')
+    cy.get('.ingredient-buttons').should('have.length', 12)
+      .get('.ingredient-buttons[name="sofritas"]').click()
+      .get('p').contains('sofritas')
+      .get('.ingredient-buttons[name="jalapenos"]').click()
+      .get('p').contains('jalapenos')
+      .get('.ingredient-buttons[name="queso fresco"]').click()
+      .get('p').contains('queso fresco')
+  })
+
+  it('should be able to submit a new burrito order once form is filled out', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 201,
+       body: {
+        "name": "Chez",
+        "ingredients": ["sofritas", "jalapenos", "queso fresco"]
+      }
+    })
+    cy.get('form')
+      .get('input[name="name"]')
+      .type('Chez')
+      .get('.ingredient-buttons[name="sofritas"]').click()
+      .get('.ingredient-buttons[name="jalapenos"]').click()
+      .get('.ingredient-buttons[name="queso fresco"]').click()
+      .get('.submit-user-order-button').click()
+      .get('.order-container')
+      .get('.order').should('have.length', 2)
+      .get('.order').last().contains('h3', 'Chez')
+      .get('.order').last().contains('li', 'sofritas')
+      .get('.order').last().contains('li', 'jalapenos')
+      .get('.order').last().contains('li', 'queso fresco')
+  })
+
+  
+
+})
